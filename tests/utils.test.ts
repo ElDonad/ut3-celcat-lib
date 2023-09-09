86d32fadd15b3545222a36ee5009cc866b4b2bf3
ElDonad <elidoset@gmail.com>
2023-09-09 16:25:41 +0200

    fix test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -2,5 +2,5 @@ import { CelcatDate } from "../src/utils"
 
 test("format date", () => {
     const date = new CelcatDate(2001, 5, 15);
-    expect(date.toCelcatStr() == "2001-06-15");
+    expect(date.toCelcatStr() == "2001-06-15").toBeTruthy();
 })
